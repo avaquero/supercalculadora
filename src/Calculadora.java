546d55bc16b3c612fe4410af8dc290c666da45d1
adrian <avaquero149@gmail.com>
2013-10-29 12:05:22 +0100

    primer commit

---
+++ src/Calculadora.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		String numero ="";
+		
+		Scanner llegir = new Scanner(System.in);
+        System.out.println("Entra numeros:");
+        numero = llegir.nextLine();
+        
+        while (numero == "="){
+        	numero += numero;
+        	numero = llegir.nextLine();
+        }
+        
+        System.out.println(numero);
+	}
+
+}
